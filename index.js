0c43d1b48d1258be68d3c5586f53ba20b9d9cffd
Rashonda Edwards <rashondaedwards@aol.com>
2023-03-02 21:46:10 -0800

    gamepart2

--- index.js
+++ index.js
@@ -10,10 +10,18 @@ function newImage(url, left, bottom){
 
 function newItem(url, left, bottom){
     let item = newImage(url, left, bottom)
+    item.addEventListener('click', function(){
+        item.remove()
+        let inventoryItem = document.createElement('img')
+        inventoryItem.src = url
+        inventory.append(inventoryItem)     // Not sure what this does...
+    })                                      // Not sure what any of this does actually...
 }
 
+let inventory = document.createElement('div')
+
 function newInventory(){
-    let inventory = document.createElement('div')
+    
     inventory.style.position = 'fixed'
     inventory.style.bottom = '0px';
     inventory.style.left = '0px'
